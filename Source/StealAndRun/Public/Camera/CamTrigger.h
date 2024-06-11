4bf2c6cf65803ca596fa1b97ca5f6538d1efd702
Nicola <nicola.latella02@gmail.com>
2024-06-11 18:04:29 +0200

    Trigger Complete

--- Source/StealAndRun/Public/Camera/CamTrigger.h
+++ Source/StealAndRun/Public/Camera/CamTrigger.h
@@ -3,8 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CamManager.h"
-#include "Components/BoxComponent.h"
 #include "GameFramework/Actor.h"
 #include "CamTrigger.generated.h"
 
@@ -15,24 +13,45 @@ class STEALANDRUN_API ACamTrigger : public AActor
 
 public:
 	// Camera target position variables, editable in the Inspector
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Adjust the distance of the camera from the player"))
 	float CamX;
 
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Adjust the side position of the camera"))
 	float CamY;
 
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Adjust the height of the camera"))
 	float CamZ;
 
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
-	float CamZDeadZone;
-
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Adjust the side DeadZone"))
 	float CamYDeadZone;
 
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Adjust the height DeadZone"))
+	float CamZDeadZone;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera", meta = (ToolTip = "Duration of the transition from PosA to PosB"))
+	float LerpDuration;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	FVector OldStartPos;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	float OldZDeadZone;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	float OldYDeadZone;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	float OldFixedZ;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	float OldFixedY;
+	
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
 	bool TriggerUsed;
 
-	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
 	bool IsInTrigger;
+
+	UPROPERTY(Blueprintable,BlueprintReadWrite, Category="Camera")
+	bool Forward;
 };
